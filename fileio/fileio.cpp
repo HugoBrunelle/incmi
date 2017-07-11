8673b3332074e71d0de5594224fbe1d0083ff342
Hugo Brunelle <ghugobrunelle@gmail.com>
2017-07-11 01:53:28 -0400

    FileIO added: getFilteredNames() and constructor

--- fileio/fileio.cpp
+++ fileio/fileio.cpp
@@ -6,10 +6,50 @@
 
 // Static methods, no overide required:
 
+FileIO::FileIO() {
+    // List all the filter flags
+    m_filterFlags = {
+        { "dirs", QDir::Dirs },
+        { "alldirs", QDir::AllDirs },
+        { "files", QDir::Files },
+        { "drives", QDir::Drives },
+        { "nosymlinks", QDir::NoSymLinks },
+        { "nodotanddotdot", QDir::NoDotAndDotDot },
+        { "nodot", QDir::NoDot },
+        { "nodotdot", QDir::NoDotDot },
+        { "allentries", QDir::AllEntries },
+        { "readable", QDir::Readable },
+        { "writable", QDir::Writable },
+        { "Executable", QDir::Executable },
+        { "modified", QDir::Modified },
+        { "hidden", QDir::Hidden },
+        { "system", QDir::System },
+        { "casesensitive", QDir::CaseSensitive }
+    };
+
+    // List all the sort flags
+    m_sortFlags = {
+        { "name", QDir::Name },
+        { "time", QDir::Time },
+        { "size", QDir::Size },
+        { "type", QDir::Type },
+        { "unsorted", QDir::Unsorted },
+        { "nosort", QDir::NoSort },
+        { "dirsfirst", QDir::DirsFirst },
+        { "dirslast", QDir::DirsLast },
+        { "reversed", QDir::Reversed },
+        { "ignorecase", QDir::IgnoreCase },
+        { "localeaware", QDir::LocaleAware }
+    };
+}
+
+FileIO::~FileIO() {
+
+}
+
 QString FileIO::getApplicationPath(){
     return QDir::currentPath();
 }
-
 
 // All methods linked to the working directory
 QString FileIO::readFile(const QString &filename)
@@ -97,6 +137,25 @@ bool FileIO::dirExist(const QString& dirname){
 
 void FileIO::resetDirectory(){
     cDir = QDir::current();
+}
+
+QStringList FileIO::getFilteredNames(const QString& filterString,const QString& sortString)
+{
+    // Initialise filter and sort with default value
+    QDir::Filter filter = QDir::AllEntries;
+    QDir::SortFlag sortFlag = QDir::NoSort;
+
+    // If the filter exist apply it
+    if(m_filterFlags.find(filterString) != m_filterFlags.end()) {
+        filter = m_filterFlags.at(filterString);
+    }
+
+    // If the sort flag exist apply it
+    if(m_sortFlags.find(sortString) != m_sortFlags.end()) {
+        sortFlag = m_sortFlags.at(sortString);
+    }
+
+    return cDir.entryList(filter, sortFlag);
 }
 
 
--- fileio/fileio.h
+++ fileio/fileio.h
@@ -9,6 +9,8 @@ class FileIO: public QObject
     Q_OBJECT
 
 public:
+    Q_INVOKABLE FileIO();
+    Q_INVOKABLE ~FileIO();
     Q_INVOKABLE QString readFile(const QString &filename);
     Q_INVOKABLE bool writeFile(const QString &data, const QString &filename);
     Q_INVOKABLE bool removeFile(const QString &filename);
@@ -25,6 +27,7 @@ public:
     Q_INVOKABLE bool dirExist(const QString &dirname);
     Q_INVOKABLE QString getCurrentDirName();
     Q_INVOKABLE void resetDirectory();
+    Q_INVOKABLE QStringList getFilteredNames(const QString& filterString,const QString& sortString);
     QString sourceDir() { return sDir; };
 
 public slots:
@@ -33,6 +36,8 @@ private:
         QString sDir;
         QDir cDir = QDir::current();
         bool removeDirectoryFromDir(QDir dir);
+        QDir::Filter m_filterFlags;
+        QDir::SortFlag m_sortFlags;
 };
 
 #endif // FILEIO_H
